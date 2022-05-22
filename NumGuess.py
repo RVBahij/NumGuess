aa4ae3b766f30240b5477e8ecd901cbaa149dda4
Bahij <78605044+RVBahij@users.noreply.github.com>
2022-05-22 12:46:07 +0300

    Add files via upload

---
+++ NumGuess.py
@@ -0,0 +1,36 @@
+import random
+import os
+import time
+
+def clear():
+    os.system("clear")
+    print()
+
+x = random.randint(1, 10)
+
+clear()
+time.sleep(1)
+print("Guess The Number!")
+print("Number range 1 > 9\n")
+print("You have 4 guess :)\n")
+
+
+time.sleep(6)
+
+
+for i in range(1, 5):
+    clear()
+    y = int(input(f"Enter guess number {i}: "))
+    if y == x:
+        print("\nGG! You won!\n\n")
+        break
+    elif y != x:
+        print("\nWrong guess...\n\n")
+        time.sleep(2)
+
+if x != y:
+    clear()
+    print("You lost :(\n")
+    print(f"The answer was {x}!\n\n")
+
+
